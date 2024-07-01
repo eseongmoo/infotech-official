a53d00364c1e834c55c95ed04defab3bf8d6c97c
eseongmoo <eseongmoo@gmail.com>
2024-07-01 11:04:16 +0900

    setting update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,9 +7,9 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
+		href: "#",
+		label: "",
+		handle: "@",
 	},
 	{
 		icon: <Mail size={20} />,
